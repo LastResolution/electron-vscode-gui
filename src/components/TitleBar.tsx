050fe9572d9cb117e276e6c22f8f878ffd627200
LifeViolation <128378329+LifeViolation@users.noreply.github.com>
2023-07-17 13:46:52 +0900

    add: AppIcon

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
 import "../css/TitleBar.css";
 import { ControlButton } from "./ControlButton";
+import { AppIcon } from "./AppIcon";
 
 export const TitleBar = () => {
   useEffect(() => {});
   return (
     <nav id="title-bar">
-      <div id="empty"></div>
+      <AppIcon />
       <div id="title">App.tsx - electron-vs-code-gui - Visual Studio Code</div>
       <ControlButton />
     </nav>
