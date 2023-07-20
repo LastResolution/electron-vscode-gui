ada2945ffcc397cf7c70594fe048685d5d43b747
LifeViolation <128378329+LifeViolation@users.noreply.github.com>
2023-07-20 12:35:56 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
+import { MainContent } from "./components/MainContent";
 import { TitleBar } from "./components/TitleBar";
 
 function App() {
   return (
     <div className="App">
       <TitleBar />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>version: {window.electronAPI.version()}</div>
-      </header> */}
+      <MainContent />
     </div>
   );
 }
